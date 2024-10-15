dcb932be161abe52c68be5b45f251e460007e716
djbp17 <daven.piloton17@gmail.com>
2024-10-15 17:42:48 +0800

    Done working on the Jobs Page. Started working on Add Job Page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import MainLayout from './layouts/MainLayout';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
 const router = createBrowserRouter(
   // createRoutesFromElements(<Route path="/about" element={<h1>My App</h1>} />)
@@ -17,6 +19,9 @@ const router = createBrowserRouter(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      {/* The ":id" in the example below is not a literal ID that you are passing  in. It is just a symbol or a variable for whatever the ID is*/}
+      <Route path="/add-job" element={<AddJobPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
-// import jobs from '../jobs.json'; This was the old code before using a json server
+// import jobs from '../jobs.json'; This was the old code before using a json server where we are fetching data from a file not in the server
 import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
+import Spinner from './Spinner';
 
 // const JobListings = () => { OLD
 // console.log(jobs); we do console logs for checking if it really outputs of what we need
 // const recent = jobs.slice(0, 3); OLD
 const JobListings = ({ isHome = false }) => {
-  // const jobListings = isHome ? jobs.slice(0, 3) : jobs; old code
+  // const jobListings = isHome ? jobs.slice(0, 3) : jobs; old code showing only 3 jobs ay HomePage
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // useEffect was used to fetch data. The example below is just one of the example of fetching data(Base way of fetching from client side component). There is also another way of fetching data using SWR(Please check this third-party library if goods na ka) it is much easier compare using useEffect base on Brad
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -15,11 +35,16 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
-        </div>
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
