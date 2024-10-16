8707695e43bc9bcadeda72832c4b0b612c534a7e
djbp17 <daven.piloton17@gmail.com>
2024-10-16 17:21:51 +0800

    Finish the React Job Listing Project 2 Yay!

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,61 @@ import MainLayout from './layouts/MainLayout';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
-const router = createBrowserRouter(
-  // createRoutesFromElements(<Route path="/about" element={<h1>My App</h1>} />)
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      {/* The ":id" in the example below is not a literal ID that you are passing  in. It is just a symbol or a variable for whatever the ID is*/}
-      <Route path="/add-job" element={<AddJobPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
 const App = () => {
+  // Add new Job
+  const addjob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = fetch(`/api/jobs/${id}`, { method: 'DELETE' });
+    return;
+  };
+
+  // Update Job
+  const updatedJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    // createRoutesFromElements(<Route path="/about" element={<h1>My App</h1>} />)
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        {/* The ":id" in the example below is not a literal ID that you are passing  in. It is just a symbol or a variable for whatever the ID is*/}
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addjob} />} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updatedJob} />}
+          loader={jobLoader}
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />;
 };
 
